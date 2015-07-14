82a75a9b9ec21db7f60d7b89ebb14fb577a215cf
Andrew French <andrew@afren.ch>
2015-07-14 14:13:49 -0800

    Reinitialize registers after Evaluate button pressed.

--- main.js
+++ main.js
@@ -3,13 +3,14 @@
 // Parser.parse(Test.command_list);
 
 document.getElementById("evaluate").onclick = function() {
+  Registers.reinitialize();
+
   var code_lines = document.getElementById("main").value.split("\n");
   Parser.parse(code_lines);
 
   for(var i = 0; i < register_array.length; i++) {
     var hex_value = Registers[register_array[i]].value.toString(16);
-    hex_value = Array(9 - hex_value.length).join("0") + hex_value;
-    hex_value = "0x" + hex_value;
+    hex_value = "0x" + Array(9 - hex_value.length).join("0") + hex_value;
     document.getElementById(register_array[i]).innerHTML = hex_value;
   }
 }
--- registers.js
+++ registers.js
@@ -34,172 +34,212 @@ var register_array = [
   "$gp",
   "$sp",
   "$fp",
-  "$ra"
+  "$ra",
+  "PC"
 ];
 
 var Registers = {
   PC: {
       mutable: true,
+      init: 0x00400000,
       value: 0x00400000
   },
 
   $zero: {
     mutable: false,
+    init: 0,
     value: 0
   },
 
   $at: {
     mutable: false,
+    init: 0,
     value: 0
   },
 
   $v0: {
     mutable: true,
+    init: 0,
     value: 0
   },
 
   $v1: {
     mutable: true,
+    init: 0,
     value: 0
   },
 
   $a0: {
     mutable: true,
+    init: 0,
     value: 0
   },
 
   $a1: {
     mutable: true,
+    init: 0,
     value: 0
   },
 
   $a2: {
     mutable: true,
+    init: 0,
     value: 0
   },
 
   $a3: {
     mutable: true,
+    init: 0,
     value: 0
   },
 
   $t0: {
     mutable: true,
+    init: 0,
     value: 0
   },
 
   $t1: {
     mutable: true,
+    init: 0,
     value: 0
   },
 
   $t2: {
     mutable: true,
+    init: 0,
     value: 0
   },
 
   $t3: {
     mutable: true,
+    init: 0,
     value: 0
   },
 
   $t4: {
     mutable: true,
+    init: 0,
     value: 0
   },
 
   $t5: {
     mutable: true,
+    init: 0,
     value: 0
   },
 
   $t6: {
     mutable: true,
+    init: 0,
     value: 0
   },
 
   $t7: {
     mutable: true,
+    init: 0,
     value: 0
   },
 
   $s0: {
     mutable: true,
+    init: 0,
     value: 0
   },
 
   $s1: {
     mutable: true,
+    init: 0,
     value: 0
   },
 
   $s2: {
     mutable: true,
+    init: 0,
     value: 0
   },
 
   $s3: {
     mutable: true,
+    init: 0,
     value: 0
   },
 
   $s4: {
     mutable: true,
+    init: 0,
     value: 0
   },
 
   $s5: {
     mutable: true,
+    init: 0,
     value: 0
   },
 
   $s6: {
     mutable: true,
+    init: 0,
     value: 0
   },
 
   $s7: {
     mutable: true,
+    init: 0,
     value: 0
   },
 
   $t8: {
     mutable: true,
+    init: 0,
     value: 0
   },
 
   $t9: {
     mutable: true,
+    init: 0,
     value: 0
   },
 
   $k0: {
     mutable: false,
+    init: 0,
     value: 0
   },
 
   $k1: {
     mutable: false,
+    init: 0,
     value: 0
   },
 
   $gp: {
     mutable: true,
+    init: 0,
     value: 0
   },
 
   $sp: {
     mutable: true,
+    init: 0,
     value: 0
   },
 
   $fp: {
     mutable: true,
+    init: 0,
     value: 0
   },
 
   $ra: {
     mutable: true,
+    init: 0,
     value: 0
+  },
+
+  reinitialize: function() {
+    for(var i = 0; i < register_array.length; i++) {
+      this[register_array[i]].value = this[register_array[i]].init;
+    }
   }
 };
