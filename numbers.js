6045e9bb38094b4c7b04bb3f579509541b31f6dc
Andrew French <andrew@afren.ch>
2015-07-18 00:11:13 -0800

    Negative numbers now kind of supported.

--- numbers.js
+++ numbers.js
@@ -3,8 +3,8 @@
 var Numbers = {
   to_hex_string: function(dec_value) {
     // Converts decimal value to 32-bit hex value
-    
-    var hex_value = dec_value.toString(16);
+
+    var hex_value = (dec_value >>> 0).toString(16);
     return "0x" + Array(9 - hex_value.length).join("0") + hex_value;
   }
 }
